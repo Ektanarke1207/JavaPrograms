46f5b5734c940dd5f32385d722f92c241d0891d2
Ektanarke1207 <68851907+Ektanarke1207@users.noreply.github.com>
2021-02-23 15:04:54 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,79 @@
+package com.PrachiAssignment;
+
+class A
+
+{
+
+     
+
+}
+
+ 
+
+class B extends A
+
+{
+
+     
+
+}
+
+ 
+
+class C extends B
+
+{
+
+     
+
+}
+
+ 
+
+public class MainClass 
+
+{
+
+    static void overloadedMethod(A a)
+
+    {
+
+        System.out.println("ONE");
+
+    }
+
+     
+
+    static void overloadedMethod(B b)
+
+    {
+
+        System.out.println("TWO");
+
+    }
+
+     
+
+    static void overloadedMethod(C obj)
+
+    {
+
+        System.out.println("THREE");
+
+    }
+
+     
+
+    public static void main(String[] args)
+
+    {
+
+        C c = new C();
+
+         
+
+        overloadedMethod(c);
+
+    }
+
+}
